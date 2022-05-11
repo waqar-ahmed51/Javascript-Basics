19ffe9eb5b37643420b4c5620c698e26b785c395
Waqar Ahmed <54082156+waqarvikey75@users.noreply.github.com>
2022-05-11 20:02:05 +0500

    Create string_interpolation.js
    Code examples on how String interpolation works in javascript.

---
+++ string_interpolation.js
@@ -0,0 +1,12 @@
+//String Interpolation Example
+const myPet = 'armadillo';
+console.log(`I own a pet ${myPet}.`);
+// Output: I own a pet armadillo.s
+
+// a template literal is wrapped by backticks `
+
+//Example 2
+var myName='Waqar Ahmed';
+var myCity='Shikarpur';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`);
+
