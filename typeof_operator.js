44f8d74d47afeb0f2ad39c87ab5f7e54d865a9f0
Waqar Ahmed <54082156+waqarvikey75@users.noreply.github.com>
2022-05-11 21:48:46 +0500

    Create typeof_operator.js

---
+++ typeof_operator.js
@@ -0,0 +1,16 @@
+//The typeof operator checks the value to its right and returns, or passes back, a string of the data type.
+const unknown1 = 'foo';
+console.log(typeof unknown1); // Output: string
+ 
+const unknown2 = 10;
+console.log(typeof unknown2); // Output: number
+ 
+const unknown3 = true; 
+console.log(typeof unknown3); // Output: boolean
+
+//Example
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+//Changing the value of varible
+newVariable=1;
+console.log(typeof newVariable);
