a115954a089da0f4a9ea9dc65dfc33d651435f4d
Waqar Ahmed <54082156+waqarvikey75@users.noreply.github.com>
2022-05-27 15:58:03 +0500

    Conditional Statements

---
+++ conditional_statement.js
@@ -0,0 +1,27 @@
+//If condition example
+let sale=true;
+if(sale){
+  console.log('Time to buy!');
+}
+
+//If else example
+if (false) {
+    console.log('The code in this block will not run.');
+  } else {
+    console.log('But the code in this block will!');
+  }
+  // Prints: But the code in this block will!
+
+
+  //Example with operator
+let hungerLevel=7;
+if(hungerLevel>=7){
+  console.log('Time to eat!');
+}else{
+  console.log('We can eat later!');
+}
+
+
+
+
+
