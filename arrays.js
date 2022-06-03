6e2be400d8cd1da0727443ffa0580f3c0a379192
Waqar Ahmed <54082156+waqarvikey75@users.noreply.github.com>
2022-06-03 18:42:00 +0500

    Arrays and Nested Arrays revised

--- arrays.js
+++ arrays.js
@@ -17,6 +17,56 @@ console.log(listItem);
 
 console.log(famousSayings[2]);
 
-
 console.log(famousSayings[3]); // will output undefined
+
+// Update Arrays
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+ 
+seasons[3] = 'Autumn';
+console.log(seasons); 
+//Output: ['Winter', 'Spring', 'Summer', 'Autumn']
+
+
+// Length of the Array
+const newYearsResolutions = ['Keep a journal', 'Take a falconry class'];
+ 
+console.log(newYearsResolutions.length);
+// Output: 2
+
+
+
+// Push method to add an element in the last.
+const itemTracker = ['item 0', 'item 1', 'item 2'];
+ 
+itemTracker.push('item 3', 'item 4');
+ 
+console.log(itemTracker); 
+// Output: ['item 0', 'item 1', 'item 2', 'item 3', 'item 4'];
+
+
+//Pop method to remove the last element in arrays
+const chores = ['wash dishes', 'do laundry', 'take out trash', 'cook dinner', 'mop floor'];
+
+chores.pop();
+
+console.log(chores);
+
+
+// More methods in Javascript for Arrays
+const groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+
+groceryList.shift();
+
+console.log(groceryList);
+
+groceryList.unshift('popcorn');
+
+console.log(groceryList);
+console.log(groceryList.slice(1,4));
+console.log(groceryList);
+
+const pastaIndex=groceryList.indexOf('pasta');
+console.log(pastaIndex);
+
+
 
---
+++ nested-arrays.js
@@ -0,0 +1,5 @@
+let numberClusters=[[1,2],[3,4],[5,6]];
+
+const target=numberClusters[2][1];
+
+console.log(target);
